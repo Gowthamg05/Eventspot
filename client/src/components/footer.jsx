a3668819edf7019aee971dbc49b3421da14a7e08
Gowthamg05 <g.gowtham11052003@gmail.com>
2025-09-09 11:36:40 +0530

    Made header and footer mobile responsive

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="bg-gray-200 text-center p-4 mt-8">
-    <p className="text-sm text-black-600">© 2025 Event Portal. All rights reserved.</p>
-  </footer>
+<footer className="bg-gray-200 text-center p-4 mt-8">
+  <p className="text-sm text-gray-700 sm:text-base">
+    © 2025 Event Portal. All rights reserved.
+  </p>
+</footer>
 );
 
 export default Footer;
